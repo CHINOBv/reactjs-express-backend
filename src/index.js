028804c09bdfbb0e0effe39e2c891d57ebb18d96
CHINOBv <retrochanel97@gmail.com>
2020-06-08 03:20:34 -0700

    Servidor basico iniciado

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const config = require("./server/config.js");
 require("./db.js");
 
-const app = config(express(),express);
+const app = config(express());
 
 app.listen(app.get('port'), () => {
   console.log("Server Its Runing On Port: "+ app.get('port'));
--- src/server/config.js
+++ src/server/config.js
@@ -1,26 +1,50 @@
 const cors = require("cors");
 const path = require("path");
 const exphbs = require("express-handlebars");
-module.exports = (app,express) => {
+const morgan = require("morgan");
+const multer = require("multer");
+const express = require("express");
+const routes = require("../routes/index.routes.js");
+const errorHandler = require("errorhandler");
 
+module.exports = (app) => {
   //Settings
-  app.set('port', process.env.PORT || 5000);
-  app.set("views", path.join(__dirname,"views"));
-  app.engine(".hbs", exphbs({
-    defaultLayout: 'main',
-    partialsDir: path.join(app.get('views'),'partials'),
-    layoutsDir: path.join(app.get('views','layouts')),
-    extname: '.hbs',
-    helpers: require("./helpers.js")
-  }))
+  app.set("port", process.env.PORT || 5000);
+  app.set("views", path.join(__dirname, "views"));
+  app.engine(
+    ".hbs",
+    exphbs({
+      defaultLayout: "main",
+      partialsDir: path.join(app.get("views"), "partials"),
+      layoutsDir: path.join(app.get("views"), "layouts"),
+      extname: ".hbs",
+      helpers: require("./helpers.js"),
+    })
+  );
+  app.set("view engine", ".hbs");
+
   //middlewares
 
   app.use(cors());
-  app.use(express.json())
+  app.use(express.json());
+  app.use(morgan("dev"));
+  app.use(
+    multer({ dest: path.join(__dirname, "../public/upload/temp") }).single(
+      "image"
+    )
+  );
+  app.use(express.urlencoded( {extended: false} ));
 
   //Routes
+  routes(app);
+
+  //Static Files
+  app.use('/public', express.static(path.join(__dirname,'../public')))
 
   //Errors
+  if('dev' === app.get('env')){
+    app.use(errorHandler)
+  }
 
   return app;
 };
