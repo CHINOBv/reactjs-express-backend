b9b655f8b300cb7747ae6b22ef1e06b275e26fe1
CHINOBv <retrochanel97@gmail.com>
2020-06-08 03:56:34 -0700

    Routes Ready's

---
+++ src/controllers/image.js
@@ -0,0 +1,22 @@
+const ctrl = {};
+
+ctrl.Index = (req, res) => {
+  
+};
+
+ctrl.Create = (req, res) => {
+  
+};
+ctrl.Like = (req, res) => {
+  
+};
+ctrl.Comment = (req, res) => {
+  
+};
+
+ctrl.Delete = (req, res) => {
+  
+};
+
+
+module.exports = ctrl;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,7 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+const home = require("../controllers/home.js");
+const image = require('../controllers/image.js');
+
 module.exports = app =>{
 
-  app.get('/', (req,res) => {
-    res.send("index page")
-  });
+  router.get('/', image.Index);
+  router.get('/images/:image_id', image.Index);
+  router.post('/images', image.Create);
+  router.post('/images/:image_id/like', image.Like);
+  router.post('/images/:image_id/comment', image.Comment);
+  router.delete('/images/:image_id/delete', image.Delete);
 
+  app.use(router);
 };
