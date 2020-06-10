608999550ffc40fccc322b6815d7884581806190
CHINOBv <retrochanel97@gmail.com>
2020-06-10 04:08:29 -0700

    Resolved the problem with Names

--- src/helpers/libs.js
+++ src/helpers/libs.js
@@ -1,7 +1,7 @@
 const helper = {};
 
 helper.randomNumber = () => {
-  const posible = 'a1b2c3d4e5f6g7h8i9j0kl8m7n6ñ5o4p3q2r1s0t4u5v2w65x78y86z100$-_*#=?';
+  const posible = 'a1b2c3d4e5f6g7h8i9j0kl8m7n6ñ5o4p3q2r1s0t4u5v2w65x78y86z100';
 
   let rNumber = 0;
 
